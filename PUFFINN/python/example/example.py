ca7282158d061011c65f34ccede6d885fa2250fd
Martin Aumüller <maau@itu.dk>
2019-06-27 11:34:12 +0000

    updated Python example

--- PUFFINN/python/example/example.py
+++ PUFFINN/python/example/example.py
@@ -4,27 +4,35 @@ import time
 import math
 
 d = 100
-n = 100000
+n = 10000
 k = 10
 n_queries = 100
-s = 1 << 27 # ~ 128mb
+s = 1 << 24 # ~ 16mb
 
 
-i = Index('angular', d, s, hash_function = 'simhash', hash_source = 'independent')
+index = Index('angular', d, s, hash_function = 'simhash', hash_source = 'independent')
 
 print('Creating %d points (d = %d) and %d queries' % (n, d, n_queries))
-dataset = [[numpy.random.normal(0, 1) / math.sqrt(d) for _ in range(d)]  for _ in range(n)]
-queries = [[numpy.random.normal(0, 1) / math.sqrt(d) for _ in range(d)]  for _ in range(n_queries)]
+dataset = [[numpy.random.normal(0, 1) for _ in range(d)]  for _ in range(n)]
+queries = [[numpy.random.normal(0, 1) for _ in range(d)]  for _ in range(n_queries)]
+
+for v in dataset:
+    for i in range(len(v)):
+        v[i] /= numpy.linalg.norm(v)
+        
+for v in queries:
+    for i in range(len(v)):
+        v[i] /= numpy.linalg.norm(v)
 
 print('Computing ground truth')
 ground_truth = [sorted([numpy.dot(q, v) for v in dataset])[-9:] for q in queries]
 
 print('Building index')
 for v in dataset:
-    i.insert(v)
+    index.insert(v)
 
 t0 = time.time()
-i.rebuild()
+index.rebuild()
 print("Building index took %.2f seconds." % (time.time() - t0) )
 
 
@@ -32,7 +40,7 @@ results = []
 print('Searching the index')
 t0 = time.time()
 for query in queries:
-    results.append(i.search(query, k, 0.5, 'none'))
+    results.append(index.search(query, k, 0.9999, 'none'))
 
 print("Search the index took %.2f seconds." % (time.time() - t0))
 
